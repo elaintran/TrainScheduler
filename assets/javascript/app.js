cf9d2c6e4477babd3e91ad000016131ed02c793b
elaintran <elain.ttran@gmail.com>
2019-06-11 12:36:33 -0500

    basic pseudocode for js

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//create an array or object to place the input in
+//when the modal form is filled out
+//form on submit
+//get the values of the form
+//put into array
+//append to table
+//maybe check letter casing and rewrite in camelcase
+//check if numbers are in the right format
+//if not, give an error message
+//store the information in firebase
