704824b3d712ca70b187cd376116d7e851145098
elaintran <elain.ttran@gmail.com>
2019-06-11 21:07:13 -0500

    broke down JS into functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@
 //store the information in firebase
 
 //firebase config
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyBw9TjCqqffgXP-TKUUFiNQlbSIoqf2drQ",
     authDomain: "minimal-train-scheduler.firebaseapp.com",
     databaseURL: "https://minimal-train-scheduler.firebaseio.com",
@@ -15,25 +15,44 @@ const firebaseConfig = {
     appId: "1:560193805039:web:6b9344bb4c960f9f"
 };
 
+firebase.initializeApp(config);
+var database = firebase.database();
+
 //global variables
-var nameList = [];
-var destinationList = [];
-var frequencyList = [];
+var trainName;
+var destination;
+var firstTrainTime;
+var frequency;
 
 //when form is submitted, get values
 $(".schedule-form").on("submit", function(event) {
+    //prevent page from refreshing
     event.preventDefault();
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTrainTime = $("#first-train-time").val().trim();
-    var frequency = $("#frequency").val().trim();
+    //get values from form fields and trim the white space
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrainTime = $("#first-train-time").val().trim();
+    frequency = $("#frequency").val().trim();
+    //add train to schedule
+    addTrain();
+    //clear all fields
+    clearForm();
+})
+
+function addTrain() {
     //append a new row containing the form input to table
     var rowElement = $("<tr>");
-    var nameCell = $("<td>").text(trainName);
+    var nameCell = $("<td>").addClass("train-name").text(trainName);
     var destinationCell = $("<td>").text(destination);
     var frequencyCell = $("<td>").text(frequency);
-    rowElement.append(nameCell).append(destinationCell).append(frequencyCell);
+    var deleteCell = $("<td>");
+    var deleteIcon = $("<i>").addClass("fas fa-trash");
+    deleteCell.append(deleteIcon);
+    rowElement.append(nameCell).append(destinationCell).append(frequencyCell).append(deleteCell);
     $("tbody").append(rowElement);
+}
+
+function clearForm() {
     //if none of the form fields are empty
     if (trainName !== "" && destination !== "" && firstTrainTime !== "" && frequency !== "") {
         //clear values on form
@@ -44,4 +63,4 @@ $(".schedule-form").on("submit", function(event) {
         //hide modal on submit
         $("#modal").modal("hide");
     }
-})
+}
